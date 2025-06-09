f8cbede491996e068235ebb3b51f231dc2cf6bfe
Rhys Stever <rhys@stever.com>
2025-06-09 19:13:44 -0400

    Spawn enemies every 2 seconds; keep player centered

---
+++ Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField]
+    private float spawnRange;
+    [SerializeField]
+    private GameObject basicEnemyPrefab;
+
+    private float spawnTimer, currentSpawnTimer;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        spawnTimer = 2f;
+        currentSpawnTimer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate() {
+        if(GameManager.instance.CurrentGameState == GameState.Game) {
+            currentSpawnTimer += Time.deltaTime;
+
+            if(currentSpawnTimer >= spawnTimer) {
+                SpawnEnemy(basicEnemyPrefab);
+                currentSpawnTimer = 0f;
+            }
+        }
+    }
+
+    public void SpawnEnemy(GameObject enemy) {
+        float randomAngle = Random.Range(0, 360f);
+        Vector2 newPosition = new Vector2(Mathf.Sin(randomAngle), Mathf.Cos(randomAngle));
+        newPosition *= spawnRange;
+
+        Instantiate(enemy, newPosition, Quaternion.identity, transform);
+    }
+}
